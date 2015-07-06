9acd79d2620545448f4a0ee07acefe42f793dc76
unknown <rkenfacktsakem@DWKA-FRW7G5J.united.domain>
2015-07-06 12:20:55 +0200

    Added create TouchEvent method for manually trigerring touch events

--- src/event/Emitter.js
+++ src/event/Emitter.js
@@ -1,8 +1,10 @@
 import eventHelpers from "src/event/Helpers";
 import cssHelpers from "src/css/Helpers";
 import Logger from "src/modules/Logger";
+import utils from "src/utils/Utils";
 
-export default (function() {
+export
+default (function () {
   //item.dispatchEvent(new CustomEvent(eventName ,{ detail : data }));
   //http://www.w3.org/TR/pointerevents/#pointerevent-interface
   /*var event = new PointerEvent("pointerover",
@@ -15,77 +17,218 @@ export default (function() {
     });
 eventTarget.dispatchEvent(event); */
 
-  var setDefaults = function(properties) {
-    var defaults = {
-      bubbles : true,
+
+//https://gist.github.com/basecss/8666646
+
+
+  var eventProperties = {
+
+    base: {
+      bubbles: true,
       cancelable: true
-    };
-    for(var prop in defaults) {
-      if(typeof properties[prop] == "undefined") {
-        roperties[prop] = defaults[prop];
+    },
+
+    mouse : {
+      canBubble: true,
+      cancelable: true,
+      view: window,
+      detail: 0,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      button: 0,
+      relatedTarget: null
+    },
+
+    touch : {
+      touches : [],
+      targetTouches : [],
+      changedTouches : [],
+      altKey : false,
+      metaKey : false,
+      ctrlKey : false,
+      shiftKey : false
+    },
+
+    pointer : {
+      canBubble: true,
+      cancelable: true,
+      view: window,
+      detail: 0,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      button: 0,
+      relatedTarget: null,
+      offsetX : 0,
+      offsetY : 0,
+      width : 0,
+      height : 0,
+      pressure : 0,
+      rotation : 0,
+      tiltX : 0,
+      tiltY : 0,
+      pointerId : 0,
+      pointerType : null,
+      hwTimestamp : 0,
+      isPrimary : false
+    },
+
+
+    initTouch : {
+      canBubble: true,
+      cancelable: true,
+      view: window,
+      detail: 0,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      touches: [],
+      targetTouches: [],
+      changedTouches: [],
+      scale: 1,
+      rotation: 0,
+      touchItem: 0
+    }
+
+  };
+
+
+  var setDefaults = function (type, properties) {
+    var res = {};
+    for (var prop in eventProperties[type]) {
+      if(eventProperties[type].hasOwnProperty(prop)) {
+        if (typeof properties[prop] == "undefined") {
+          res[prop] = eventProperties[type][prop];
+        } else {
+          res[prop] = properties[prop];
+        }
       }
     }
-    return properties;
-  };
-
-  var isMouseEvent = function(eventName) {
-    return (eventName == "click") || (eventName == "dbclick") || (eventName.indexOf("mouse") == 0);
-  };
-
-  var isTouchEvent = function(eventName) {
-    return eventName.indexOf("touch") == 0;
-  };
-
-  var isPointerEvent = function(eventName) {
-    return eventName.indexOf("pointer") == 0;
+    return res;
   };
 
 
-  var createBaseEvent = function(eventName, properties) {
+  var createTouch = function(target) {
+    return {
+      view : window,
+      target : target,
+      identifier : utils.getUID(),
+      pageX : 0,
+      pageY : 0,
+      screenX : 0,
+      screenY : 0
+    };
+  };
+
+
+  var addDefaultTouches = function(target, eventName, properties) {
+    var touch = createTouch(target);
+    if(properties.touches.length === 0) {
+      properties.touches.push(touch);
+    }
+    if(properties.targetTouches.length === 0) {
+      properties.targetTouches.push(touch);
+    }
+    if(properties.changedTouches.length === 0) {
+      properties.changedTouches.push(touch);
+    }
+  };
+
+
+  var isMouseEvent = function (eventName) {
+    return (eventName == "click") || (eventName == "dbclick") || (eventName.indexOf("mouse") === 0);
+  };
+
+
+  var isTouchEvent = function (eventName) {
+    return eventName.indexOf("touch") === 0;
+  };
+
+
+  var isPointerEvent = function (eventName) {
+    return eventName.indexOf("pointer") === 0;
+  };
+
+
+  var createBaseEvent = function (eventName, properties) {
     var evt = null;
-    properties = setDefaults(properties);
-    if(typeof Event != "undefined") {
+    properties = properties || {};
+    properties = setDefaults("base", properties);
+    if (typeof Event != "undefined") {
       evt = Event(eventName, properties);
     } else {
       evt = document.createEvent('Event');
-      evt.initEvent(eventName, true, true);
+      evt.initEvent([eventName].concat(Object.values(properties)));
     }
     return evt;
   };
 
 
-  var createMouseEvent = function(eventName, properties) {
+  var createMouseEvent = function (eventName, properties) {
     var evt = null;
-    if(typeof MouseEvent != "undefined") {
-      var evt = new MouseEvent(eventName, {
-        bubbles : true,
-        cancelable: true,
-        view: window,
-      });
+    properties = properties || {};
+    properties = setDefaults("mouse", properties);
+    if (typeof MouseEvent != "undefined") {
+      evt = new MouseEvent(eventName, properties);
     } else {
       evt = document.createEvent('MouseEvent');
-      evt.initMouseEvent(eventName, true, true);
+      evt.initMouseEvent.apply([eventName].concat(Object.values(properties)));
     }
     return evt;
   };
 
 
-  var createTouchEvent = function(eventName, properties) {
-
+  var createTouchEvent = function (target, eventName, properties) {
+    var evt = null;
+    properties = properties || {};
+    properties = setDefaults("touch", properties);
+    properties = addDefaultTouches(target, eventName, properties);
+    if (typeof TouchEvent != "undefined") {
+      evt = new TouchEvent(eventName, properties);
+    } else {
+      evt = document.createEvent('TouchEvent');
+      evt.initTouchEvent.apply(evt, [eventName].concat(Object.values(properties)));
+    }
+    return evt;
   };
 
-  var createPointerEvent = function(eventName, properties) {
 
+  var createPointerEvent = function (eventName, properties) {
+    var evt = null;
+    properties = properties || {};
+    properties = setDefaults("pointer", properties);
+    if (typeof PointerEvent != "undefined") {
+      evt = new PointerEvent(eventName, properties);
+    } else {
+      evt = document.createEvent('PointerEvent');
+      evt.initPointerEvent.apply(evt, [eventName].concat(Object.values(properties)));
+    }
+    return evt;
   };
 
 
-
-  var createEvent(eventName) {
-    if(isMouseEvent(eventName)) {
+  var createEvent = function (item, eventName, properties) {
+    if (isMouseEvent(eventName)) {
       return createMouseEvent(eventName);
-    } else if(isTouchEvent(eventName)) {
-      return createTouchEvent(eventName);
-    } else if(isPointerEvent(eventName)) {
+    } else if (isTouchEvent(eventName)) {
+      return createTouchEvent(item, eventName, properties);
+    } else if (isPointerEvent(eventName)) {
       return createPointerEvent(eventName);
     } else {
       return createBaseEvent(eventName);
@@ -95,31 +238,30 @@ eventTarget.dispatchEvent(event); */
 
   return {
 
-
-    emitNative : function(eventName, properties) {
-
-      properties = properties || {};
-
-      this.forEach(function(item) {
-        if(eventHelpers.isEventSupported(item, eventName)) {
-          var evt = createEvent(eventName, properties);
+    emitNative : function (eventName, properties) {
+      this.forEach(function (item) {
+        if (eventHelpers.isEventSupported(item, eventName)) {
+          var evt = createEvent(item, eventName, properties);
           item.dispatchEvent(evt);
         } else {
-          Logger.error("dispatchEvent not supported on "+el);
+          Logger.error(eventName+" not supported on " + item);
         }
       });
       return this;
     },
 
 
-    emitCustom : function(eventName, data) {
+    emitCustom : function (eventName, data) {
       data = data || {};
-      this.forEach(function(item) {
-        item.dispatchEvent(new CustomEvent(eventName ,{ detail : data }));
+      this.forEach(function (item) {
+        item.dispatchEvent(new CustomEvent(eventName, {
+          detail: data
+        }));
       });
       return this;
     }
 
-  }
+
+  };
 
 })();
--- src/polyfill/Object.js
+++ src/polyfill/Object.js
@@ -1,8 +1,8 @@
 export default (function () {
 
-  if (!Object.assign) {
+  if (!window.Object.assign) {
 
-    Object.defineProperty(Object, 'assign', {
+    Object.defineProperty(window.Object, 'assign', {
       enumerable : false,
       configurable : true,
       writable : true,
@@ -11,18 +11,18 @@ export default (function () {
         if (target === undefined || target === null) {
           throw new TypeError('Cannot convert first argument to object');
         }
-        var to = Object(target);
+        var to = window.Object(target);
         for (var i = 1; i < arguments.length; i++) {
           var nextSource = arguments[i];
           if (nextSource === undefined || nextSource === null) {
             continue;
           }
-          nextSource = Object(nextSource);
+          nextSource = window.Object(nextSource);
 
-          var keysArray = Object.keys(Object(nextSource));
+          var keysArray = window.Object.keys(window.Object(nextSource));
           for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
             var nextKey = keysArray[nextIndex];
-            var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+            var desc = window.Object.getOwnPropertyDescriptor(nextSource, nextKey);
             if (desc !== undefined && desc.enumerable) {
               to[nextKey] = nextSource[nextKey];
             }
@@ -33,6 +33,62 @@ export default (function () {
     });
   }
 
-  return true;
+
+  if (!window.Object.keys) {
+    window.Object.keys = (function() {
+      'use strict';
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [], prop, i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
+}
+
+
+if (!window.Object.values) {
+
+  window.Object.values = (function(obj) {
+    'use strict';
+    return function(obj) {
+      var values = [];
+      window.Object.keys(obj).forEach(function(key, index) {
+        values[index] = obj[key];
+      });
+      return values;
+    };
+  })();
+}
+
 
 })();
