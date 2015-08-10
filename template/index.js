00085ec6451ef74f18404be8a48f1f1b1ff8f3e1
Romeo Kenfack <tsakem@yahoo.fr>
2015-08-10 18:57:59 +0200

    Implemented template dispose

--- template/index.js
+++ template/index.js
@@ -1,11 +1,11 @@
-var Template = function (node, scope) {
+var Template = function(node, scope) {
   node.$$template = {
     node: node.cloneNode(true),
     instance: this
   };
-  this.original = node.cloneNode(true),
+  this.original = node.cloneNode(true);
   this.node = node;
-  this.render(node, scope);
+  this._render(node, scope);
 };
 
 
@@ -41,31 +41,66 @@ Template.regex = {
 };
 
 
+Template.create = function(node, model) {
+  if (!node.$$template) {
+    return new Template(node, model);
+  } else {
+    var parent = node.parentNode;
+    var oldNode = node;
+    var instance = node.$$template.instance;
+    node = node.$$template.node;
+    node.$$template = {
+      node: node.cloneNode(true),
+      instance: instance
+    };
+    node.$$template.instance._removeListeners.call(node.$$template.instance);
+    node.$$template.instance._render.call(node.$$template.instance, node, model);
+    parent.replaceChild(node, oldNode);
+    return instance;
+  }
+};
+
+
 Template.prototype = {
 
-  node : null,
-  original : null,
+  node: null,
+  original: null,
 
   _listeners: [],
 
-  dispose : function () {
+  dispose : function() {
     this._removeListeners();
+    var node = this.node;
+    var parent = node.parentNode;
+    parent.replaceChild(this.node.$$template.node, node);
     delete this.node.$$template;
   },
 
-  update : function(model) {
-    this.render(this.original, model);
+  update: function(model) {
+    var node = this.node;
+    var parent = node.parentNode;
+    var oldNode = node;
+    var instance = node.$$template.instance;
+    this.node = node = node.$$template.node;
+    node.$$template = {
+      node: node.cloneNode(true),
+      instance: instance
+    };
+    node.$$template.instance._removeListeners.call(node.$$template.instance);
+    node.$$template.instance._render.call(node.$$template.instance, node, model);
+    parent.replaceChild(node, oldNode);
+    return instance;
   },
 
-  _removeListeners : function() {
-    this._listeners.forEach(function (registered) {
+  _removeListeners: function() {
+    this._listeners.forEach(function(registered) {
       registered.node.removeEventListener(registered.eventName, registered.listener, false);
     });
     this._listeners = [];
   },
 
 
-  _isEventSupported: function (target, eventName) {
+  _isEventSupported: function(target, eventName) {
     eventName = 'on' + eventName;
     var isSupported = (eventName in target);
     if (!isSupported) {
@@ -77,10 +112,10 @@ Template.prototype = {
   },
 
 
-  _getExpressions: function (value) {
+  _getExpressions: function(value) {
     var expressions = [];
     var test = value.match(Template.regex.token) || [];
-    test.forEach(function (match) {
+    test.forEach(function(match) {
       expressions.push({
         templExp: match.trim(),
         paramName: (match.match(Template.regex.paramName)[0]).trim()
@@ -91,7 +126,7 @@ Template.prototype = {
   },
 
 
-  _parseRepeatExpression: function (value) {
+  _parseRepeatExpression: function(value) {
     var expression = null;
     var test = value.match(Template.regex.repeat);
     if (test) {
@@ -104,7 +139,7 @@ Template.prototype = {
   },
 
 
-  _getPathValue: function (obj, path) {
+  _getPathValue: function(obj, path) {
 
     var parts = path.split(".");
     var res = obj;
@@ -126,12 +161,12 @@ Template.prototype = {
   },
 
 
-  _renderTextNode: function (node, scope) {
+  _renderTextNode: function(node, scope) {
     var textContent = node.textContent;
     if (textContent.length > 0) {
       var expressions = this._getExpressions(textContent);
       var parts = null;
-      expressions.forEach(function (expression) {
+      expressions.forEach(function(expression) {
         textContent = textContent.replace(expression.templExp, this._getPathValue(scope, expression.paramName));
       }.bind(this));
       node.textContent = textContent;
@@ -139,13 +174,13 @@ Template.prototype = {
   },
 
 
-  _getParamList: function (scope, funcString) {
+  _getParamList: function(scope, funcString) {
     var startPos = funcString.indexOf("(");
     var endPos = funcString.indexOf(")");
     var funcName = funcString.substr(0, startPos);
     var params = funcString.substr(startPos + 1, endPos - startPos - 1).trim();
     if (params.length > 0) {
-      params = params.split(",").map(function (param) {
+      params = params.split(",").map(function(param) {
         return this._getPathValue(scope, param.trim());
       }.bind(this));
     } else {
@@ -158,8 +193,8 @@ Template.prototype = {
   },
 
 
-  _callFunction: function (funcString, args) {
-    return (function () {
+  _callFunction: function(funcString, args) {
+    return (function() {
       var ref = this;
       var parts = funcString.split(".");
       for (var i = 0; i < parts.length; i++) {
@@ -169,7 +204,7 @@ Template.prototype = {
           throw "The function " + funcString + " is not defined";
         }
       }
-      return function () {
+      return function() {
         ref.apply(this, args);
       }
     })();
@@ -177,11 +212,11 @@ Template.prototype = {
   },
 
 
-  _renderAttributeNode: function (refNode, node, scope) {
+  _renderAttributeNode: function(refNode, node, scope) {
     var attrValue = node.value;
     var expressions = this._getExpressions(attrValue);
     if (expressions.length) {
-      expressions.forEach(function (expression) {
+      expressions.forEach(function(expression) {
         attrValue = attrValue.replace(expression.templExp, this._getPathValue(scope, expression.paramName));
       }.bind(this));
     } else {
@@ -213,18 +248,18 @@ Template.prototype = {
   },
 
 
-  _renderAttributes: function (node, scope) {
+  _renderAttributes: function(node, scope) {
     var attrs = node.attributes;
     if (attrs && attrs.length > 0) {
       attrs = Array.prototype.slice.call(attrs);
-      attrs.forEach(function (attr) {
+      attrs.forEach(function(attr) {
         this._renderAttributeNode(node, attr, scope);
       }.bind(this));
     }
   },
 
 
-  render: function (node, scope) {
+  _render: function(node, scope) {
 
     var children;
 
@@ -250,58 +285,36 @@ Template.prototype = {
       var fragments = [];
       children = Array.prototype.slice.call(node.childNodes);
       var fragement = document.createDocumentFragment();
-      children.forEach(function (child) {
+      children.forEach(function(child) {
         fragement.appendChild(child);
       });
       fragments.push(fragement);
       for (var i = 1; i < l; i++) {
         var subScope = null;
         fragement = document.createDocumentFragment();
-        children.forEach(function (child) {
+        children.forEach(function(child) {
           fragement.appendChild(child.cloneNode(true));
         });
         fragments.push(fragement);
       }
 
-      fragments.forEach(function (fragement, index) {
+      fragments.forEach(function(fragement, index) {
         var subScope = {};
         subScope[repeatExpression.paramName] = data[index];
-        this.render(fragement, subScope);
+        this._render(fragement, subScope);
         node.appendChild(fragement);
       }.bind(this));
 
     } else {
       children = Array.prototype.slice.call(node.childNodes);
-      children.forEach(function (child) {
+      children.forEach(function(child) {
         if (child.nodeType == Template.NODE_TYPE.TEXT) {
           this._renderTextNode(child, scope);
         } else if (child.nodeType == Template.NODE_TYPE.ELEMENT) {
           this._renderAttributes(child, scope);
-          this.render(child, scope);
+          this._render(child, scope);
         }
       }.bind(this));
     }
   }
-};
-
-
-var template = function (node, model) {
-
-  if (!node.$$template) {
-    return new Template(node, model);
-  } else {
-    var parent = node.parentNode;
-    var oldNode = node;
-    var instance = node.$$template.instance;
-    node = node.$$template.node;
-    node.$$template = {
-      node : node.cloneNode(true),
-      instance : instance
-    };
-    node.$$template.instance._removeListeners.call(node.$$template.instance);
-    node.$$template.instance.render.call(node.$$template.instance, node, model);
-    parent.replaceChild(node, oldNode);
-    return instance;
-  }
-
 };
